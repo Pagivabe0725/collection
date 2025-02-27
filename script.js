6a5a70b5878977f978e92a107839cc3c6996f427
Kovács <kovpatrik0725@gmail.com>
2025-02-27 23:44:46 +0100

    javascript-update(collection-creator-inputs)

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const collectionCreatorDisplayButton = document.getElementById(
 const collectionCreatorSection = document.getElementById(
   "own-collection-creator"
 );
-
 collectionCreatorDisplayButton.addEventListener("click", () => {
   collectionCreatorDisplayHandler();
 });
+
+const collectionCreatorCreateButton = document.getElementById(
+  "own-collection-creator-create-button"
+);
+collectionCreatorCreateButton.addEventListener("click", () => {
+  getValueOfInputFieldsOfCollectionCreatorSection();
+});
+
+Array.from(collectionCreatorSection.getElementsByTagName("input")).forEach(
+  (element, index) => {
+    element.addEventListener("click", () => {
+      removeemphasisOfInfalidInputFields(index);
+    });
+  }
+);
 
 /**
  * This function checks the display property of the collection creator section.
@@ -65,4 +79,72 @@ function collectionCreatorDisplayHandler() {
   collectionCreatorDisplayOrNot();
   setCollectionCreatorDisplayButtonValue();
   resetCollectionCreatorInputs();
+}
+
+/**
+ * Returns an array of invalid HTML input elements.
+ * If no invalid elements are found, returns an empty array.
+ * @returns {Array<Element>} An array of invalid input elements, or an empty array if none are found.
+ */
+
+function getInvalidInputFieldsOfCollectionCreatorSection() {
+  let helperArray = [];
+  Array.from(collectionCreatorSection.getElementsByTagName("input")).forEach(
+    (element) => {
+      if (element.validity.valueMissing) {
+        helperArray.push(element);
+      }
+    }
+  );
+  return helperArray;
+}
+
+/**
+ * emphasis the invalid input fields provided in parameter
+ * @param {Array<Element>} inputs An array of HTML input elements 
+ */
+
+function emphasisInvalidInputFields(inputs) {
+  inputs.forEach((elemet) => {
+    elemet.classList.add("form-control");
+    elemet.classList.add("is-invalid");
+    elemet.classList.add("bg-danger-subtle");
+  });
+}
+
+/**
+ * Removes the emphasis from an invalid input field.
+ * @param {Number} index The index of the current input element
+ */
+
+function removeemphasisOfInfalidInputFields(index) {
+  let input = Array.from(
+    collectionCreatorSection.getElementsByTagName("input")
+  )[index];
+  input.classList.remove("form-control");
+  input.classList.remove("is-invalid");
+  input.classList.remove("bg-danger-subtle");
+}
+
+/**
+ * Returns an array of input values if they are valid
+ * @returns {Array<string>} An array of valid input values
+ */
+function getValueOfInputFieldsOfCollectionCreatorSection() {
+  console.log("bentvan");
+  const invalidInputs = getInvalidInputFieldsOfCollectionCreatorSection();
+  if (invalidInputs.length === 0) {
+    console.log("megfelel");
+    let helperArray = [];
+    Array.from(collectionCreatorSection.getElementsByTagName("input")).forEach(
+      (element) => {
+        helperArray.push(element.value);
+      }
+    );
+    console.log(helperArray);
+    return helperArray;
+  } else {
+    console.log("helytelen");
+    emphasisInvalidInputFields(invalidInputs);
+  }
 }
